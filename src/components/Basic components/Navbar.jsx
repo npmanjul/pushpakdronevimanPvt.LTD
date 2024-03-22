07dd55bcb13a00f9781fa2a0661b2634092f506a
anjulsingh999 <anjulsingh462@gmail.com>
2024-03-23 01:10:19 +0530

    ok

--- src/components/Basic components/Navbar.jsx
+++ src/components/Basic components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import './nav.css';
-import {NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import logo from './img/footer logo.png';
 import MenuIcon from '@mui/icons-material/Menu';
 import Training_dropdown from './Training_dropdown';
@@ -9,46 +9,82 @@ import Product_dropdown from "./Product_dropdown";
 
 const Navbar = () => {
 
+    function openNav() {
+        document.getElementById("myNav").style.width = "100%";
+        setTimeout(() => {
+            document.getElementById("menu_line").style.display = "none";
+        }, 300)
+
+    }
+
+    /* Close when someone clicks on the "x" symbol inside the overlay */
+    function closeNav() {
+        document.getElementById("myNav").style.width = "0%";
+        document.getElementById("menu_line").style.display = "block";
+        console.log("ok")
+    }
+
     return (
         <>
             <div className="navbar">
                 <div className="logo_section">
-                    <img src={logo}/>
+                    <img src={logo} />
                 </div>
                 <div className="navigation">
                     <div className="home_section">
                         <NavLink to='/' className="link">Home</NavLink>
                     </div>
                     <div className="dropdown_box">
-                    <NavLink to='/training' className="link">Training</NavLink>
+                        <NavLink to='/training' className="link">Training</NavLink>
                         <div className="dropdown_boxes">
-                        <Training_dropdown/>
+                            <Training_dropdown />
                         </div>
                     </div>
                     <div className="dropdown_box">
-                    <NavLink to='/services' className="link">Services</NavLink>
+                        <NavLink to='/services' className="link">Services</NavLink>
                         <div className="dropdown_boxes ">
-                        <Services_dropdown/>
+                            <Services_dropdown />
                         </div>
                     </div>
                     <div className="dropdown_box">
-                    <NavLink to='/product' className="link">Product</NavLink>
+                        <NavLink to='/product' className="link">Product</NavLink>
                         <div className="dropdown_boxes ">
-                        <Product_dropdown/>
+                            <Product_dropdown />
                         </div>
                     </div>
                     <div className="R&D_section ">
-                    <NavLink to='/rd' className="link">R&D</NavLink>
+                        <NavLink to='/rd' className="link">R&D</NavLink>
                     </div>
                     <div className="about_us_section " >
-                    <NavLink to='/aboutus' className="link">About Us</NavLink>
+                        <NavLink to='/aboutus' className="link">About Us</NavLink>
                     </div>
                 </div>
                 <div className="contact_section">
-                <NavLink to='/contactus' className="linkcontactus"> <button>Contact Us</button></NavLink>  
+                    <NavLink to='/contactus' className="linkcontactus"> <button>Contact Us</button></NavLink>
                 </div>
                 <div className='menu-section'>
-                <button ><MenuIcon/></button>
+                    <button onClick={openNav} id="menu_line" ><MenuIcon /></button>
+                </div>
+            </div>
+
+            <div class="Curtain_Menu">
+
+                <div id="myNav" class="overlay">
+
+                    <a href="javascript:void(0)" className="closebtn" onClick={closeNav}>&times;</a>
+
+                    <div class="overlay-content">
+                    <NavLink to='/' className="link" onClick={closeNav}>Home</NavLink>
+                    <NavLink to='/training' className="link" onClick={closeNav}>Training</NavLink>
+                    <NavLink to='/services' className="link" onClick={closeNav}>Services</NavLink>
+                    <NavLink to='/product' className="link" onClick={closeNav}>Product</NavLink>
+                    <NavLink to='/rd' className="link" onClick={closeNav}>R&D</NavLink>
+                    <NavLink to='/aboutus' className="link" onClick={closeNav}>About Us</NavLink>
+                        <div className="contact_section-menu">
+                            <NavLink to='/contactus' className="linkcontactus" onClick={closeNav}> <button>Contact Us</button></NavLink>
+                        </div>
+                    </div>
+
                 </div>
             </div>
 
