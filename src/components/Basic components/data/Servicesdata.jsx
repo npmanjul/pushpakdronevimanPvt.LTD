6b1ba401807f5d4d71e4af77e7b8013fa067e318
anjulsingh999 <anjulsingh462@gmail.com>
2024-03-28 17:37:49 +0530

    add images

--- src/components/Basic components/data/Servicesdata.jsx
+++ src/components/Basic components/data/Servicesdata.jsx
@@ -5,37 +5,37 @@ const nulltext=" Lorem ipsum dolor sit amet, consectetur adipiscing elit.Sed non
 const sdata=[
     {
         title: "Agriculture Drone Applications",
-        imgsrc: nullimg,
+        imgsrc: "https://firebasestorage.googleapis.com/v0/b/pushpak-drone-viman-pvt-ltd.appspot.com/o/Services%2FWhatsApp%20Image%202023-11-07%20at%2011.19.14_ef5a9957.jpg?alt=media&token=580ffcb6-8341-4c3e-b415-824a54ecf0bd",
         contant: " By incorporating agriculture drones into Pushpak Drone, we aim to provide innovative solutions for modern farming, emphasizing precision farming, efficient pesticide and fertilizer application, as well as time and cost savings. Our commitment extends to promoting sustainability and empowering farmers and rural communities, enabling them to make data-driven decisions while fostering economic growth and environmental stewardship."
     },
     {
         title: "Mapping & Surveillance",
-        imgsrc: nullimg,
+        imgsrc: "https://firebasestorage.googleapis.com/v0/b/pushpak-drone-viman-pvt-ltd.appspot.com/o/Services%2Fmapping%26%20servillancing.jpg?alt=media&token=af748ad6-1006-43c9-923f-08405272129a",
         contant:nulltext
     },
     {
         title: "Aerial Photography & Videography",
-        imgsrc: nullimg,
+        imgsrc: "https://firebasestorage.googleapis.com/v0/b/pushpak-drone-viman-pvt-ltd.appspot.com/o/Services%2FIMG-20231124-WA0003.jpg?alt=media&token=bde3fd54-c18d-495d-98d5-1410af824472",
         contant:nulltext
     },
     {
         title: "Drone Assembly",
-        imgsrc: nullimg,
+        imgsrc: "https://firebasestorage.googleapis.com/v0/b/pushpak-drone-viman-pvt-ltd.appspot.com/o/Services%2FDrone%20Assembly.jpg?alt=media&token=f5020b0d-8071-49f8-b74f-2a9c3599bbc4",
         contant: nulltext
     },
     {
         title: "Drone Testing",
-        imgsrc: nullimg,
+        imgsrc: "https://firebasestorage.googleapis.com/v0/b/pushpak-drone-viman-pvt-ltd.appspot.com/o/Services%2Fdrone%20testing.jpg?alt=media&token=775adfef-1a0a-4c49-a641-b35785b39ec6",
         contant: nulltext
     },
     {
         title: "Drone Repair Service",
-        imgsrc: nullimg,
+        imgsrc: "https://firebasestorage.googleapis.com/v0/b/pushpak-drone-viman-pvt-ltd.appspot.com/o/Services%2FWhatsApp%20Image%202024-01-23%20at%204.53.24%20PM.jpeg.jpg?alt=media&token=026273e7-a49a-4a56-8e13-dcd502b5a320",
         contant: nulltext
     },
     {
         title: "Testing and Calibration",
-        imgsrc: nullimg,
+        imgsrc: "https://firebasestorage.googleapis.com/v0/b/pushpak-drone-viman-pvt-ltd.appspot.com/o/Services%2Ftesting%20and%20callibration.jpg?alt=media&token=36f59498-987e-41c6-bbcb-c32ccccde5d0",
         contant: nulltext
     },
 ]
--- src/components/Basic components/data/productdata.jsx
+++ src/components/Basic components/data/productdata.jsx
@@ -6,22 +6,22 @@ const nulltext=" Lorem ipsum dolor sit amet, consectetur adipiscing elit.Sed non
 const pdata=[
     {
             title: "Agriculture Drone",
-            imgsrc: nullimg,
+            imgsrc: "https://firebasestorage.googleapis.com/v0/b/pushpak-drone-viman-pvt-ltd.appspot.com/o/Product%2FAgriculture%20Drone.jpg?alt=media&token=6f8b50e6-0b94-40b1-a5e3-dea3cf190026",
             contant:"Leading the way in agricultural precision, I offer assembled and tested spraying drones through my startup at competitive rates. Delivering optimal solutions to ensure your fields thrive,plus enjoy a complimentary 6-month service for sustained success 1. Precision Drones  2. Reliable Solutions 3. Competitive Rates 4. Thriving Fields 5. Complimentary Service "
     },
     {
             title: "Surveillance Drone",
-            imgsrc: nullimg,
+            imgsrc: "https://firebasestorage.googleapis.com/v0/b/pushpak-drone-viman-pvt-ltd.appspot.com/o/Product%2Fsrvillancedrone.jpg?alt=media&token=b10f62c4-9880-40cb-83fa-e34181e08296",
             contant:" Elevating security with innovation! Our startup specializes in surveillance drones on 3D printed frames, providing customers with cutting-edge solutions at competitive rates.Your safety, our priority!  1. Innovative Security   2. Surveillance Drones  3. 3D Printed Frames  4. Cutting-edge Solutions 5. Priority Safety"
     },
     {
             title: "Videography Drone",
-            imgsrc: nullimg,
+            imgsrc: "https://firebasestorage.googleapis.com/v0/b/pushpak-drone-viman-pvt-ltd.appspot.com/o/Product%2Fmapping%26%20servillancing.jpg?alt=media&token=71cbde70-ea97-43ca-ab41-1737b2206c5b",
             contant:nulltext
     },
     {
             title: "Mapping Drone",
-            imgsrc: nullimg,
+            imgsrc: "https://firebasestorage.googleapis.com/v0/b/pushpak-drone-viman-pvt-ltd.appspot.com/o/Product%2Fmapping%20drone.jpg?alt=media&token=19c57232-8f5a-475e-96f6-479924b38039",
             contant:"  Mapping drones navigate the skies with precision, utilizing advanced technology to create detailed and accurate maps. Equipped with specialized sensors, they survey landscapes, monitor terrain changes, and contribute to urban planning and environmental management. These unmanned aerial vehicles revolutionize mapping processes, providing efficient and high-resolution data for various industries. 1. Precision Mapping  2. Landscape Survey  3. Urban Planning"
     },
     {
@@ -31,7 +31,7 @@ const pdata=[
     },
     {
             title: "Customised Objects",
-            imgsrc: nullimg,
+            imgsrc: "https://firebasestorage.googleapis.com/v0/b/pushpak-drone-viman-pvt-ltd.appspot.com/o/Product%2Fcustomobject.jpg?alt=media&token=fe07d378-eb5b-479a-9aaa-e7271b1e3d20",
             contant:"  Embarking on my startup journey, I specialize in crafting customized products through 3D printing,tailoring each creation to match the unique needs of my customers. 1. Startup Crafting 2. Customized Products 3. 3D Printing 4. Tailored Creations 5. Unique Solutions"
     },
     {
@@ -41,7 +41,7 @@ const pdata=[
     },
     {
             title: "3D Printed objects",
-            imgsrc: nullimg,
+            imgsrc: "https://firebasestorage.googleapis.com/v0/b/pushpak-drone-viman-pvt-ltd.appspot.com/o/Product%2Fprinted%20object.jpg?alt=media&token=16b3e089-a8ff-4d7b-ab52-1669addfd229",
             contant:" Turning moments into treasures with my startup! Specializing in 3D printed gifts like Lord Ganesh idols,Lord Adiyogi replicas, and practical items like mobile stands. Personalized perfection for every occasion!  1.Your vision, our creation  2.Custom Designs  3.3D Printing 4.Innovation"
     },
     {
